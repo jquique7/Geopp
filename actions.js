02ac904d382b756c66129f2fc336a430823cbe8e
jquique7 <jquique7@hotmail.com>
2013-12-04 20:19:26 -0600

    prueba
    numero1

---
+++ actions.js
@@ -0,0 +1,40 @@
+// acciones
+$(function(){
+	  document.addEventListener("deviceready",function(){	
+	   navigator.geolocation.getCurrentPosition(function(pos){ //funcion pos
+	     initialize(pos.coords.latitude,pos.coords.longitude);  
+	   }, function(err){
+		  alert('Error: '+err.code); 
+	   });
+	  }, false);
+});
+
+
+function initialize(lat, lon) { //lat, lon: paramestros de latitud y longitud
+				//Posición del mapa
+				var latlng = new google.maps.LatLng(lat, lon);//latitud,longitud, a este obj le pasamos las coordenadas
+				var myOptions = {
+					zoom: 16,   //zoom
+					center: latlng,  //centro: latitud y longitud
+					mapTypeId: google.maps.MapTypeId.ROADMAP   // el mapa de google para que se vea 
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);//obj tipo mapa, crea el canvas
+				//Marcador
+				var marker = new google.maps.Marker({ //obj tipo marcador, atributo position, map y tittle "mi posicion
+					position: latlng, 
+					map: map, 
+					title:"Mi posición" //tittle "mi posicion"
+				});
+				var marker = new google.maps.Marker({
+      position:  latlng,
+      map: map,
+      title:"Hello World!"
+	  
+  }); 
+				var mark2 = new google.maps.Marker({
+		   position:  new google.maps.LatLng(19.003611, -98.2),
+		   map: map,
+		   title: "marcador 2"
+	  });
+      
+			}
